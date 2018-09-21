84d514e85f8febc69d59478c8f01b0bc7b7bcd27
costagavras <cgavras@yahoo.ca>
2018-09-21 13:32:18 -0400

    final

--- server.rb
+++ server.rb
@@ -1,5 +1,10 @@
 require 'sinatra'
 
-get '/portfolio' do
-  erb :portfolio
+get '/gallery' do
+  redirect to('/favourites')
+end
+
+get '/favourites' do
+  @fav_links = ["http://www.wga.hu","http://www.wikipedia.org", "http://www.youtube.com", "http://www.imdb.com", "http://www.google.com"]
+  erb :favourites
 end
